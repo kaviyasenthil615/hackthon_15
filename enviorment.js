51631497a75259df285587b9c42ece6191f0184c
kaviya <kaviyasenthil615@gmail.com>
2026-07-12 15:37:48 +0530

    Create enviorment.js

---
+++ enviorment.js
@@ -0,0 +1,26 @@
+import Navbar from "../components/Navbar";
+
+
+function Environmental(){
+
+return(
+
+<div>
+
+<Navbar/>
+
+<h1>Environmental Module</h1>
+
+<h3>Carbon Emission: 40%</h3>
+<h3>Energy Usage: 60%</h3>
+<h3>Water Conservation: 70%</h3>
+<h3>Waste Management: 80%</h3>
+
+
+</div>
+
+)
+
+}
+
+export default Environmental;
